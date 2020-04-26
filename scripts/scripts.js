e0869580ee39bbf833c7129c5ad7e5e16cf895b4
zaxel <sae11.z.alex@gmail.com>
2020-04-26 15:37:31 +0100

    OurTeamSection added, slick carousel added

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,9 +3,19 @@ $(document).ready(function () {
     $(".how_we_work_card3").find(".we_wrk_img_hide_desc_p").addClass('we_wrk_active');
     //choose what paragraph show on click on card
     for (let i = 0; i < 3; i++) {
-        $('.how_we_work_card'+(i+2)).click(function(event){
+        $('.how_we_work_card'+(i+2)).mouseover(function(event){
             $(".how_we_work_container").find(".we_wrk_img_hide_desc_p").removeClass('we_wrk_active');
             $(this).find(".we_wrk_img_hide_desc_p").addClass('we_wrk_active');
         });
     }
 });
+    //Slick Carousel
+
+$(document).ready(function () {
+    $('.team_slider').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3
+    });
+});
+
