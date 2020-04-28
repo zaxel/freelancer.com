d46711681c6188e073a3b6109dd30edfd5e89a6b
zaxel <sae11.z.alex@gmail.com>
2020-04-28 02:21:46 +0100

    Our Team Slider improved

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
-    //making second card paragraph active on page load
+    //How We Work Section Start
+    //making second card paragraph active on page load 
     $(".how_we_work_card3").find(".we_wrk_img_hide_desc_p").addClass('we_wrk_active');
     //choose what paragraph show on click on card
     for (let i = 0; i < 3; i++) {
@@ -8,16 +9,54 @@ $(document).ready(function () {
             $(this).find(".we_wrk_img_hide_desc_p").addClass('we_wrk_active');
         });
     }
+    //How We Work Section End
 });
-    //Slick Carousel
-
+    //Our Team Strart
 $(document).ready(function () {
+    //Slick Carousel Start
     $('.team_slider').slick({
         infinite: true,
         slidesToShow: 3,
         centerMode: true,
-        centerPadding: '150px',
-        slidesToScroll: 1
+        centerPadding: '0px',
+        slidesToScroll: 1,
+        responsive: [
+            {
+              breakpoint: 900,
+              settings: {
+                slidesToShow: 1
+              }
+            },
+            {
+              breakpoint: 700,
+              settings: {
+                slidesToShow: 1,
+                arrows: false
+              }
+            }
+          ]
     });
-});
+    //Slick Carousel End
+    $(".slick-active").not(".slick-center").find(".team_ocupation").addClass('team_ocupation_hiden');
+    $(".slick-active").not(".slick-center").find(".team_name").addClass('team_name_moved');
+    // $(".slick-active").find(".team_ocupation").removeClass('team_ocupation_hiden');
+    $(".slick-arrow").click(function () { 
+        $(".slick-active").find(".team_ocupation").addClass('team_ocupation_hiden');
+        $(".slick-center").find(".team_ocupation").removeClass('team_ocupation_hiden');
+        $(".slick-active").find(".team_name").addClass('team_name_moved');
+        
+        $(".slick-center").find(".team_name").addClass('team_name_normal');
+        
+    });
 
+    // $(".slick-active").on('swipe', function (event, slick, currentSlide, nextSlide) { 
+    //     $(".slick-active").find(".team_ocupation").addClass('team_ocupation_hiden');
+    //     $(".slick-center").find(".team_ocupation").removeClass('team_ocupation_hiden');
+    //     $(".slick-active").find(".team_name").addClass('team_name_moved');
+        
+    //     $(".slick-center").find(".team_name").addClass('team_name_normal');
+        
+    // });
+    
+});
+//Our Team End
